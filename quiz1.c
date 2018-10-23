277ff09b627b791df34dc5b53344fd45c53238cd
ninetyfivejae <jh33836782@gmail.com>
2018-10-24 00:35:42 +0900

    Add[quiz1.c] : 최종코드 backup 기능 추가

--- quiz1.c
+++ quiz1.c
@@ -106,34 +106,82 @@ void vi_m(char **s){
     }
 }
 
+int backup(const char *name, const struct stat *status, int type){
+    
+    char buffer[BUFSIZE], file1[BUFSIZE], file2[BUFSIZE] = "./TEMP/";
+    int fd1, fd2, i, nread;
+    
+    if(type == FTW_F){
+        
+        if(strncmp((char*)name, "./TEMP", 6) == 0)
+            return 0;
+        
+        fd1 = open(name, O_RDONLY);
+        
+        strcpy(file1, name + 2);
+        
+        for(i=0;i<strlen(file1);i++){
+            if(file1[i] == '/')
+                file1[i] = '_';
+        }
+        
+        strcat(file2, file1);
+        
+        fd2 = open(file2, O_WRONLY | O_CREAT, status->st_mode&0777);
+        
+        nread = read(fd1, buffer, BUFSIZE);
+        while(nread > 0){
+            write(fd2, buffer, nread);
+            nread = read(fd1, buffer, BUFSIZE);
+        }
+    }
+    return 0;
+}
+
+void backup_m(char **s){
+    mkdir("TEMP", 0777);
+    ftw(".", backup, 1);
+}
+
 void exec_m(char **s){
     execv(s[0], s);
 }
 
-int main(int argc, char **argv){
-    
+int main(int argc, char **argv) {
     char in[50], *res[20] = {0};
-    char *inst[6] = {"cat", "cd", "cp", "mkdir", "ls", "vi"};
-    void (*f[7])(char **) = {cat_m, cd_m, cp_m, mkdir_m, ls_m, vi_m, exec_m};
+    char *inst[7] = {"cat", "cd", "cp", "mkdir", "ls", "vi", "backup"};
+    void (*f[8])(char **) = {cat_m, cd_m, cp_m, mkdir_m, ls_m, vi_m, backup_m, exec_m};
     int i;
+    pid_t pid;
     
-    while(1){
+    while (1) {
         printf("> ");
         gets(in);
         i = 0;
         res[i] = strtok(in, " ");
-        if(strcmp(res[0], "exit")==0)
+        if (strcmp(res[0], "exit") == 0)
             exit(0);
         
-        while(res[i]){
+        while (res[i]) {
             i++;
             res[i] = strtok(NULL, " ");
         }
         
-        for(i=0;i<6;i++){
-            if(!strcmp(res[0], inst[i]))
+        for (i = 0; i < 7; i++) {
+            if (!strcmp(res[0], inst[i]))
                 break;
         }
-        f[i](res);
+        
+        if (i == 1) {
+            cd_m(res);
+        } else {
+            pid = fork();
+            if (pid == 0) {
+                f[i](res);
+                exit(0);
+            } else {
+                wait(0);
+            }
+        }
     }
 }
